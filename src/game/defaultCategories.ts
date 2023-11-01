d51ee03ed1839fd6c7a86430fdd4e60f8ac45f5c
Vincent Siao <vincent@asana.com>
2023-10-31 23:12:07 -0400

    Clean up categories list

--- src/game/defaultCategories.ts
+++ src/game/defaultCategories.ts
@@ -1,116 +1,121 @@
-export const defaultCategories = `A girl’s name
-A boy’s name
-Capital cities
-Countries
-Animals
-Musical Instruments
-Flowers
-Gemstones
-Cartoon Characters
-Four letter words
-Brands
-Things on a beach
-Websites
-Cars
-Things that are sticky
-Things that you shout
-Excuses for being late
-Pet peeves
-Ice cream flavors
-Fried foods
-Bodies of water
-Halloween costumes
-Places to go on a date
-Nicknames
-Job titles
-College majors
-Languages
-Historical figures
-Celebrities
-Holidays
-Items in a gift shop
-Things on a map
-Relatives
-Things in an office
-Software
-Fears
+export const defaultCategories = `“Adulting” activities
 Apps
-Electronic devices
-Movie titles
-Book titles
-Pets
-Musical instruments
-Types of music
 Aquatic animals
-Animals in a zoo
-Things you get in the mail
-Song title
-Vacation destination
-Famous animals
-Drinks
-Hobbies
-Things in space
-Types of candy
-Ways to get from here to there
-Furniture
-Plants
-Things in a museum
-Sports teams
-Extreme sports
-Colors
-Tools
-Dangerous activities
-Things you might go viral for
-Item within eyesight
-Things that are blue
-Body parts
-Expensive items
-Snack foods
-Healthy foods
-Animals in a zoo
-Something people hate doing
-Something that surprises you
-A common lie
-Something to strive for
+Art projects
+Associated with summer
+Associated with Christmas
+At a birthday party
 Awards/ceremonies
-Fireable offenses
-Random acts of kindness
-Four letter word (in a foreign language)
-TV shows
-Crimes
-Office supplies
-Things on your work desk
-Things associated with Xmas
-Game shows
 Board games
-Video games
-Superheroes
-Pizza toppings
-World cuisine
-Things that make you laugh
-Something you keep secret
-Things you say at work
-Mythical creatures
-“Adulting” activities
-Topics you like to talk about
+Bodies of water
+Body parts
+Book titles
+Boy names
+Brands
 Breakfast foods
-Annoying movie tropes
-Things you see on a road trip
-Things you see in the news
-Things you find in nature
-Things in the sky
 Bucket list items
-Things in an airport
-Villains
+Candy
+Capital cities
+Card games
+Cars
+Cartoon Characters
+Celebrities
+Clothing
+College majors
+Colors
+Comes in pairs or sets
+Countries
+Crimes
+Dangerous activities
+Drinks
+Electronic devices
+Excuses for being late
+Expensive items
+Extreme sports
+Fears
+Fireable offenses
+Flowers
+Found in the ocean
+Four letter words
 Four syllable words
-Words with two meanings
+Fried foods
+Furniture
+Game shows
+Gemstones
+Girl names
 Good qualities for a friend
 Guilty pleasures
-Clothing
-Words associated with self-care
-Art projects
-Weather
-Things associated with summer
+Halloween costumes
+Healthy foods
+Historical figures
+Hobbies
+Holidays
+Ice cream flavors
+In a freezer
+In a grocery store
+In a museum
+In an airport
+In an office
+In space
+In the news
+In the sky
+Insects
+Items in a gift shop
+Job titles
+Languages
+Movie titles
+Musical instruments
 Musicals
+Nicknames
+Office supplies
+People you admire
+Pet peeves
+Pets
+Pizza toppings
+Places to go on a date
+Plants
+Presidents
+Pro sports team
+Random acts of kindness
+Seen on a beach
+Seen at a concert
+Seen on a map
+Seen on a menu
+Seen on a road trip
+Seen on your work desk
+Snack foods
+Software
+Something people hate doing
+Something that surprises you
+Something to strive for
+Something you keep secret
+Song title
+Sports teams
+Superheroes
+Things that are blue
+Things that are cold
+Things that are sticky
+Things that make you laugh
+Things that need to be cleaned
+Things that you shout
+Things you don't want in the house
+Things you find in nature
+Things you get in the mail
+Things you learned as a kid
 Things you look forward to
-Presidents`;
+Things you might go viral for
+Things you say at work
+Tools
+Transportation
+TV shows
+Types of music
+U.S. Cities
+Vacation destination
+Video games
+Villains
+Weather
+Websites
+Words associated with self-care
+Words with two meanings
+World cuisine
+Zoo animals`;
